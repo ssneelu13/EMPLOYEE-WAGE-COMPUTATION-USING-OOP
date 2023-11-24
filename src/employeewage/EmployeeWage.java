c2e3420c887f79603ed57015c9ef952e0c94e5e5
Neelufer <ssneelu13@gmail.com>
2023-11-25 00:10:51 +0530

    WELCOME TO EMPLOYEE COMPUTATION PROGRAM USING OOP

---
+++ src/employeewage/EmployeeWage.java
@@ -0,0 +1,7 @@
+package employeewage;
+
+public class EmployeeWage {
+    public static void main(String[]args){
+        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM USING OOP!!!");
+    }
+}
